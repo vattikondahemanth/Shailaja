e4ddf1e27aa6a594bd212d69c73c09b5eae78084
Shailaja <shailaja.python7@gmail.com>
2022-08-24 09:37:24 +1000

    third commit

--- handson/vozapp/urls.py
+++ handson/vozapp/urls.py
@@ -2,8 +2,13 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('abc/', views.content_list,name='content_list'),
+    path('home/', views.content_list,name='content_list'),
     path('pagi/', views.content_list1,name='content_list1'),
     path('colsearch/', views.column_search,name='column_search'),
+    path('ajax_infinite/', views.ajax_infinite,name='ajax_infinite'),
+    path('get_all_dd_data/', views.get_all_drop_downs_data,name='get_all_dd_data'),
+    path('get_hot_data/', views.get_hands_on_table_data,name='get_hot_data'),
+
+
 
 ]
--- handson/vozapp/views.py
+++ handson/vozapp/views.py
@@ -1,15 +1,25 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery
 from django.shortcuts import render
 from . models import Employee
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.core import serializers
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.db.models import Q
+import json
 
 # Create your views here.
 
+column_ddl_mapper = {
+    "0" : "name",
+    "1": "salary"
+
+}
 
 def content_list(request):
-    content_data=Employee.objects.all()[:1000]
+
+
+
+    content_data=Employee.objects.all().order_by('name')[:5000]
     content_data1 = list(content_data)
     content_data1.insert(0, Employee(0 , "", 0))
     return render(request, 'vozapp/content_list1.html', {'content_data': serializers.serialize('json', content_data1)})
@@ -50,15 +60,19 @@ def column_search(request):
     salary_val = int(salary_val)
     if emp_val and salary_val:
         print("both values")
-        content_data = Employee.objects.filter(Q(name__contains=emp_val) & Q(salary__contains=salary_val))[:10]
+        content_data = Employee.objects.filter(Q(name__contains=emp_val) & Q(salary__contains=salary_val)).order_by('name')[:100]
         for i in content_data:
             print(i.name,i.salary)
     elif emp_val:
         print("emp values")
-        content_data = Employee.objects.filter(Q(name__contains=emp_val))[:1000]
+        content_data = Employee.objects.filter(Q(name__contains=emp_val)).order_by('name')[:5000]
     elif salary_val:
         print("salary values")
-        content_data = Employee.objects.filter(Q(salary__contains=salary_val))[:1000]
+        content_data = Employee.objects.filter(Q(salary__contains=salary_val)).order_by('name')[:5000]
+    elif emp_val == "":
+        print("empty emp_val")
+        content_data = Employee.objects.all().order_by('name')[:5000]
+
 
     content_data1 = list(content_data)
     print("querying completed")
@@ -68,3 +82,75 @@ def column_search(request):
     # response["Access-Control-Allow-Origin"] = "*"
     # response["Access-Control-Allow-Headers"] = "*"
     # return response
+
+
+
+def ajax_infinite(request):
+    rowcount = int(request.GET.get('rowcount'))
+    rowcount=rowcount+100
+    emp=Employee.objects.all()[:rowcount]
+    return HttpResponse(serializers.serialize('json', emp), content_type='application/json')
+
+def get_all_drop_downs_data(request):
+    search_value = request.GET.get("search")
+    result = []
+    contents = {}
+    for i in column_ddl_mapper.keys():
+        contents = get_distinct_col_values(i, search_value, 0, page_offset=1000)
+        result.append(contents)
+    print('r',result)
+    return JsonResponse(result,safe=False)
+
+
+def get_distinct_col_values(drop_down_number, search_value, current_row_num, page_offset=1000):
+    contents = {}
+    filter_count = current_row_num + page_offset
+    if drop_down_number:
+        column_name = column_ddl_mapper[drop_down_number]
+        dynamic_filter = {column_name + "__icontains": search_value}
+        column_data = Employee.objects.order_by(column_name).values_list(column_name, flat=True).filter(
+            **dynamic_filter).distinct(column_name)[:filter_count]
+        contents = {
+            drop_down_number: list(column_data)
+        }
+
+    return contents
+
+
+def get_single_drop_downs_data(request):
+    drop_down_number = request.GET.get("ddl_num")
+    search_value = request.GET.get("ddl_val")
+    current_row_num = int(request.GET.get("current_row_num"))
+
+    contents = get_distinct_col_values(drop_down_number, search_value, current_row_num, page_offset=1000)
+
+    return JsonResponse(contents)
+
+
+def get_hands_on_table_data(request):
+    hot_filter_values = request.GET.get("hot_filter_values")
+    hot_filter_values = json.loads(hot_filter_values)
+    # hot_filters = {"0": ["Employee 199123", "Employee 299123", "Employee 399123", "Employee 499123", "Employee 599123", "Employee 699123", "Employee 799123", "Employee 899123", "Employee 99123", "Employee 991230", "Employee 991231", "Employee 991232", "Employee 991233", "Employee 991234", "Employee 991235", "Employee 991236", "Employee 991237", "Employee 991238", "Employee 991239", "Employee 999123"], "1": [99123]}
+    final_criterion = Q()
+    for i in hot_filter_values.keys():
+        col_filters = hot_filter_values[i]
+        criterion = Q()
+        if col_filters:
+            for j in col_filters:
+                search_val = j.split(":")[1].strip()
+                column_name = column_ddl_mapper[i]
+                dynamic_filter = {column_name + "__icontains": search_val}
+                criterion |= Q(**dynamic_filter)
+            final_criterion |= criterion
+
+    data = Employee.objects.order_by('name').filter(final_criterion)[:2000]
+    contents = serializers.serialize('json', data)
+    return HttpResponse(contents, content_type='application/json')
+
+
+
+
+
+
+
+
